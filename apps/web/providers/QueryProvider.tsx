ec5fd62d15d42686635da95147bcaedb6e6d99da
Ashhar Siddiqui <ashhar2002.6@gmail.com>
2026-06-09 14:52:18 +0530

    setup tanstack-query

---
+++ apps/web/providers/QueryProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { queryClient } from "@/lib/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { ReactNode } from "react";
+
+
+export default function  QueryProvider({
+  children,
+}: {
+  children: ReactNode;
+}){
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+}
