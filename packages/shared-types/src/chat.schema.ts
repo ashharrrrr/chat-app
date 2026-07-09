9ac2db81de3027e79c65ba0e895bb182a31ef45d
Ashhar <ashhar2002.6@gmail.com>
2026-07-09 15:53:18 +0530

    Feat: add zod schema validation and infer types for group-chat

--- packages/shared-types/src/chat.schema.ts
+++ packages/shared-types/src/chat.schema.ts
@@ -17,6 +17,48 @@ export const messageContentSchema = z.object({
   content: z.string().trim().max(200, "Message is too long"),
 });
 
+export const createGroupSchema = z.object({
+  title: z.string().trim().min(3, "Group title mustt be at least 3 characters.").max(50, "Group title must be at most 50 characters"),
+  username: z.array(z.string()).min(1, "Select at least one member.").refine(
+    (usernames) => new Set(usernames).size === usernames.length,
+    {
+      message: "Duplicate usernames are not allowed.",
+    }
+  ),
+})
+
+export const renameGroupSchema = z.object({
+  conversationId: z.string(),
+  title: z.string().trim().min(3, "Group title mustt be at least 3 characters.").max(50, "Group title must be at most 50 characters"),
+})
+
+export const addMembersSchema = z.object({
+  conversationId: z.string(),
+  usernames: z.array(z.string().min(3, "Username must be at least 3 characters").max(20, "Username must be at most 20 characters"))
+})
+
+export const removeMemberSchema = z.object({
+  conversationId: z.string(),
+  memberId: z.string(),
+})
+
+export const updateMemberRoleSchema = z.object({
+  conversationId: z.string(),
+  memberId: z.string(),
+  role: z.enum(["member", "admin"]),
+})
+
+export const leaveGroupSchema = z.object({
+  conversationId: z.string(),
+})
+
 export type createConversationInput = z.infer<typeof createConversationSchema>;
 export type sendMessageInput = z.infer<typeof sendMessageSchema>;
 export type MessageContentInput = z.infer<typeof messageContentSchema>;
+export type CreateGroupInput = z.infer<typeof createGroupSchema>;
+export type RenameGroupInput = z.infer<typeof renameGroupSchema>;
+export type AddMembersInput = z.infer<typeof addMembersSchema>;
+export type RemoveMemberInput = z.infer<typeof removeMemberSchema>;
+export type UpdateMemberRoleInput = z.infer<typeof updateMemberRoleSchema>;
+export type LeaveGroupInput = z.infer<typeof leaveGroupSchema>;
+
